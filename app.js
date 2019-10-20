2cf3e10efd309414d3bb6d94951c805e8fcfb784
NitefullWind <nitefullwind@163.com>
2019-10-20 23:23:06 +0800

    Add websocket server

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var flash = require('./project/flash')
+var config = require('./project/config');
 
 var app = express();
+
+require('./project/socket_server')(config.get('websocket.port'), config.get('websocket.tsPort'), config.get('websocket.tsHost'))
 
 // session 必须在路由之前，否则路由中无法使用session
 app.use(session({
@@ -25,6 +28,7 @@ app.use(session({
     }
 }))
 
+// 使用消息闪现
 app.use(flash())
 
 // view engine setup
---
+++ project/socket_server.js
@@ -0,0 +1,62 @@
+var logger = require('./logger').getLogger("socket_server")
+
+/**
+ * 创建一个WebSocket服务器，并将数据转发到Tcp服务器
+ * @param {number} wsPort WebSocket server's port
+ * @param {number} tsPort Tcp Server's port
+ * @param {string} tsHost Tcp Server's host
+ */
+function SocketServer(wsPort, tsPort, tsHost="127.0.0.1") {
+	var webSocket = require("socket.io")(wsPort)
+        
+	webSocket.on('connection', function(socket) {
+		logger.debug("websocket connected")
+
+		var tcpSocket = require('net').Socket()
+		tcpSocket.connect(tsPort, tsHost, () => {
+			logger.debug(`tcpsocket connecting to ${tsHost}:${tsPort}`)
+		});
+
+		socket.on('error', (error) => {
+			logger.debug("websocket error: ", error)
+		});
+
+		socket.on('disconnecting', (reason) => {
+			logger.debug("websocket disconnecting. reason: ", reason)
+			tcpSocket.end()
+		});
+
+		socket.on('message', (data) => {
+			logger.debug("websocket received message: ", data)
+			tcpSocket.write(data, callback = () => {
+				logger.debug("tcpsocket writen data: ", data, )
+			})
+		});
+
+		tcpSocket.on('close', (hadError) => {
+			if(hadError) {
+				logger.debug("tcpsocket closed by error")
+			} else {
+				logger.debug("tcpsocket closed")
+			}
+		});
+
+		tcpSocket.on('connect', () => {
+			logger.debug("tcpsocket connected")
+		});
+
+		tcpSocket.on('data', (data) => {
+			logger.debug("tcpsocket receive data: ", data)
+		});
+
+		tcpSocket.on('error', (error) => {
+			logger.debug("tcpsocket error: ", error)
+		});
+
+		tcpSocket.on('end', () => {
+			logger.debug("tcpsocket end")
+		});
+	});
+}
+
+module.exports = SocketServer
